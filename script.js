7e58cf22cc20a4a71f019dfbb50c9f54546d1405
AbdullahSaad5 <syedabdullahsaad1@gmail.com>
2021-08-02 23:09:44 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote-text");
+const authorName = document.getElementById("author-name");
+const newQuote = document.getElementById("new-quote");
+const twitterButton = document.getElementById("twitter-button");
+const loader = document.getElementById("loader");
+
+newQuote.addEventListener("click", getQuotes);
+twitterButton.addEventListener("click", tweet);
+async function getQuotes() {
+  loading();
+  const apiURL = "https://goquotes-api.herokuapp.com/api/v1/random?count=1";
+  try {
+    const response = await fetch(apiURL);
+    const queryObject = await response.json();
+    const quote = queryObject.quotes[0];
+    quoteText.innerHTML = quote.text;
+    authorName.innerHTML = quote.author;
+    if (quote.text.length > 120) {
+      quoteText.classList.add("smaller-quote-text");
+    } else {
+      quoteText.classList.remove("smaller-quote-text");
+    }
+    loaded();
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function loading() {
+  loader.style.display = "block";
+  quoteContainer.style.display = "none";
+}
+
+function loaded() {
+  loader.style.display = "none";
+  quoteContainer.style.display = "flex";
+}
+
+function tweet() {
+  const url = `https://twitter.com/intent/tweet?text=${
+    quoteText.textContent + " - " + authorName.textContent
+  }`;
+  window.open(url, "_blank");
+}
+
+getQuotes();
